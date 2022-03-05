b66d677764aaa3bad09761bcbf0f477a7bb9df42
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-03-05 17:25:41 +0100

    Dodanie POI do Infra/Repo

---
+++ SocialMap.Infrastructure/Repositories/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SocialMap.Core.Domain;
+using System.Threading.Tasks;
+
+namespace SocialMap.Infrastructure.Repositories
+{
+    public class AppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<POIAccess> POIAccess { get; set; }
+        public DbSet<POI> POI { get; set; }
+
+
+    }
+}
