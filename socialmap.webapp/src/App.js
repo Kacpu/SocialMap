b54c9efe0668f20e30a87ce3d9da7356612166c1
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-03-16 22:38:48 +0100

    Testing NavBar

--- socialmap.webapp/src/App.js
+++ socialmap.webapp/src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import './App.css';
 import Map from "./components/Map/Map";
-import Bar from "./components/Bar";
-import Footer from "./components/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import CustomFooter from "./components/Footer";
 
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+/*
+Header - Naglowek (navbar, itp)
+Content - Zawartosc (mapa, itp)
+Footer - Stopka (autorzy)
+*/
 
 function App() {
   return (
-      <div>
-        <Bar />
+    <div>
+      {/* <Layout className="layout">
+        <Header> 
+          <NavBar />
+        </Header>
+        <Content> */}
+        <NavBar/>
         <Map />
-        <Footer />
-      </div>
+        {/* </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          <CustomFooter></CustomFooter>  
+        </Footer>
+      </Layout> */}
+    </div>
   );
 }
 
