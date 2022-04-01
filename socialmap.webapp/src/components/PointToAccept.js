0c9998170c1729bb7670fd08c4f3012cff0901d6
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-04-01 21:39:59 +0200

    Moderator panel skeleton

--- socialmap.webapp/src/components/PointToAccept.js
+++ socialmap.webapp/src/components/PointToAccept.js
@@ -24,13 +24,33 @@ import {
     Center,
     Spacer
 } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import React, {useEffect, useState} from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 
 export default function PointToAccept(props) {
+    const [expand, setExpand] = useState(false);
+
+    const downIcon= () =>
+    (
+        <ChevronDownIcon align w={10} h={10} bgColor={'blue'} />
+    );
+
+    const upIcon=() =>
+    (
+        <ChevronUpIcon align w={10} h={10} bgColor={'blue'} />
+    )
+
+    const handleToggle= () => ( setExpand(!expand) );
+
 
     return (
         <Box>
-            <Stack bgColor={'gray.600'} rounded={'lg'} px={5} py={2}>
+            <Stack bgColor={'gray.600'} border={'1px'} borderColor= {'transparent'}rounded={'lg'} px={5} py={2}
+            _hover={{
+                borderColor: 'white',
+                cursor: 'pointer'
+            }}
+            onClick={handleToggle}>
                 <Flex>
                     <Box bgColor="green">
                         <HStack>
@@ -44,11 +64,24 @@ export default function PointToAccept(props) {
                         </HStack>
                     </Box>
                     <Spacer />
-                    <Box alignContet={'right'} bgColor="red">
-                        <ChevronDownIcon align w={10} h={10} />
+                    <Box display={'flex'} bgColor="red" alignItems={'center'} justifyContent={'center'} >
+                        {expand ? upIcon() : downIcon()}
                     </Box>
                 </Flex>
             </Stack>
+            <Box display={expand ? '' : 'none'}>
+                <Stack bgColor={'gray.600'}>
+                    <Text>Author</Text>
+                    <Text>Category</Text>
+                    <Text>X</Text>
+                    <Text>Y</Text>
+                    <Text>MAP</Text>
+                    <Text>Description</Text>
+                    <Text>IsGlobal</Text>
+                    <Button>Accept</Button>
+                    <Button>Reject</Button>
+                </Stack>
+            </Box>
         </Box>
     );
 }
