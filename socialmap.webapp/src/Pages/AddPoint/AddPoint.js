674c2ef2703c5eb2e3bb3cf336091e50e66e7f07
Oskar <marchewjapko@gmail.com>
2022-04-03 01:55:09 +0200

    Added map bounds, added limited external POI searching

--- socialmap.webapp/src/Pages/AddPoint/AddPoint.js
+++ socialmap.webapp/src/Pages/AddPoint/AddPoint.js
@@ -1,213 +1,257 @@
 import {
-  FormErrorMessage,
-  Badge,
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  HStack,
-  InputRightElement,
-  Stack,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-  Link,
-  Textarea,
-  Select,
-  Switch
+    Badge,
+    Box,
+    Button, Collapse,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Select,
+    Stack,
+    Switch, Table, TableContainer, Tbody, Td,
+    Text,
+    Textarea, Th, Thead, Tr,
+    useColorModeValue
 } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form'
-import { useState } from 'react';
-import { SearchIcon } from '@chakra-ui/icons';
+import {useForm} from 'react-hook-form'
+import {SearchIcon} from '@chakra-ui/icons';
 import Map from '../../components/Map/Map'
-import { withAuthenticationRequired } from '@auth0/auth0-react';
 
-import { categoryData } from '../../mocks/CategoryMock';
-
+import {categoryData} from '../../mocks/CategoryMock';
+import React, {useState} from "react";
+import {Marker, Popup} from "react-leaflet";
+import {ReactComponent as Like} from "../../icons/like-icon.svg";
+import {POIMock} from "../../mocks/POIMock";
 
 
 function InfoBadge(props) {
-  return (
-    <Box display='flex' alignItems='baseline'>
-      <Badge borderRadius='full' px='2' colorScheme='blue'>
-        {props.text}
-      </Badge>
-    </Box>
-  );
+    return (
+        <Box display='flex' alignItems='baseline'>
+            <Badge borderRadius='full' px='2' colorScheme='blue'>
+                {props.text}
+            </Badge>
+        </Box>
+    );
 }
 
 
-
 export default function AddPoint() {
+// console.log(categoryData)
+    const [value, setValue] = React.useState('')
 
-  console.log(categoryData)
+    const [items, setItems] = useState([]);
+    const [showPointList, setShowPointList] = useState(false)
 
-  const boxColor = useColorModeValue('gray.600', 'gray.700');
-  const labelColor = useColorModeValue('gray.600', 'gray.200');
-  const inputColor = useColorModeValue('gray.100', 'gray.50')
-  const subBoxColor = useColorModeValue('gray.600', 'gray.600');
+    const boxColor = useColorModeValue('gray.600', 'gray.700');
+    const labelColor = useColorModeValue('gray.600', 'gray.200');
+    const inputColor = useColorModeValue('gray.100', 'gray.50')
+    const subBoxColor = useColorModeValue('gray.600', 'gray.600');
+    const categoryList = categoryData.map((category) =>
+        <option key={category.id} value={category.id}>{category.name}</option>
+    );
 
-  const categoryList = categoryData.map((category) =>
-    <option key={category.id} value={category.id} >{category.name}</option>
-  );
+    function onSubmit(data) {
+        let obj = JSON.stringify(data, null, 3)
+//obj.isGlobal = !obj.isGlobal;
+        alert(obj)
+    }
 
-  function onSubmit(data) {
-    let obj = JSON.stringify(data, null, 3)
-    //obj.isGlobal = !obj.isGlobal;
-    alert(obj)
-  }
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isSubmitting},
+    } = useForm()
 
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting },
-  } = useForm()
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Flex
-        minH={'100vh'}
-        justify={'center'}
-        //bg={useColorModeValue('gray.50', 'gray.800')}
-        >
-
-        <Stack spacing={5} mx={'auto'} maxW={'700px'} w={'100%'} py={12} px={0}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'} textAlign={'center'} color={'gray.100'}>
-              Add new interesting Point!
-            </Heading>
-            <Text fontSize={'lg'} color={'gray.400'}>
-              save your favourite place 🌎
-            </Text>
-          </Stack>
-
-          <InfoBadge text="1. Informations" />
-
-          <Box
-            rounded={'lg'}
-            bg={boxColor}
-            boxShadow={'lg'}
-            p={8}>
-
-            <Stack spacing={5}>
-
-              <FormControl isInvalid={errors.name}>
-                <FormLabel htmlFor='name' color={labelColor}>Name</FormLabel>
-                <Input id='name' type="text" color={inputColor} bgColor={subBoxColor} 
-                placeholder='Give a wonderfull name'
-                  {...register("name", {
-                    required: "This is required",
-                    minLength: { value: 3, message: "Minimum length should be 4" }
-                  })} />
-                <FormErrorMessage>
-                  {errors.name && errors.name.message}
-                </FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={errors.description}>
-                <FormLabel htmlFor="description" color={labelColor}>Description</FormLabel>
-                <InputGroup>
-                  <Textarea id='description' color={inputColor} bgColor={subBoxColor}
-                  placeholder="Write amazing description"
-                    {...register("description", {
-                      required: "This is required",
-                      minLength: { value: 5, message: "Minimum length should be 5" }
-                    })} />
-                </InputGroup>
-                <FormErrorMessage>
-                  {errors.description && errors.description.message}
-                </FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={errors.category}>
-                <FormLabel htmlFor='category' color={labelColor}>Category</FormLabel>
-                <Select id='category' color={inputColor} bgColor={subBoxColor} placeholder='Select category'
-                  {...register("category", {
-                    required: "This is required"
-                  }
-                  )}>
-                  {categoryList}
-                </Select>
-                <FormErrorMessage>
-                  {errors.category && errors.category.message}
-                </FormErrorMessage>
-              </FormControl>
-
-              <FormControl isInvalid={errors.isPrivate} display='flex' alignItems='center'>
-                <FormLabel htmlFor='isGlobal' mb='0' color={labelColor}>Global</FormLabel>
-                <Switch id='isGlobal'
-                  {...register("isGlobal", {})} />
-                <FormErrorMessage>
-                  {errors.isGlobal && errors.isGlobal.message}
-                </FormErrorMessage>
-              </FormControl>
-
-            </Stack>
-          </Box>
-
-          <InfoBadge text="2. Pick location" />
-
-          <Box
-            rounded={'lg'}
-            bg={boxColor}
-            boxShadow={'lg'}
-            p={8}>
-
-            <Stack spacing={5}>
-              <FormControl>
-                <FormLabel htmlFor='findLocation' color={labelColor}>Location</FormLabel>
-                <InputGroup>
-                  <Input id="findLocation" type="text" color={inputColor} 
-                  placeholder='find location' />
-                  <InputRightElement h={'full'}>
-                    <Button
-                      variant={'ghost'}
-                      onClick={() =>
-                        console.log("find place")
-                      }
-                      color={'blue.300'}
-                      >
-                      <SearchIcon />
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-
-              <Map height={'400px'} />
-
-              <FormControl>
-                <Input id='location' type="text" placeholder='ul. Sample 123' disabled={true}
-                  value="Sample123"
-                  {...register("location", {})} />
-              </FormControl>
-
-            </Stack>
-          </Box>
-
-          <Stack spacing={10} pt={2}>
-            <Button
-              loadingText="Submitting"
-              size="lg"
-              bg={'blue.600'}
-              color={'white'}
-              _hover={{
-                bg: 'blue.500',
-              }}
-              width={'99%'}
-              alignSelf={'center'}
-              type="submit"
-              isLoading={isSubmitting}
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+    const handleClick = () => {
+        // fetch("open.mapquestapi.com/nominatim/v1/search.php?key=mNZPR3h0A0UeNPdXA33howJhCqJwBhwQ&format=json&q=" + value + "&addressdetails=1&limit=3&viewbox=-1.99%2C52.02%2C0.78%2C50.94&exclude_place_ids=41697")
+        //     .then(res => res.json())
+        //     .then(result => {
+        //             setItems(result);
+        //         }
+        //     )
+        // setShowPointList(true);
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Flex
+                minH={'100vh'}
+                justify={'center'}
+                //bg={useColorModeValue('gray.50', 'gray.800')}
             >
-              Add Point
-            </Button>
-          </Stack>
-        </Stack>
-      </Flex>
-    </form>
-  );
+
+                <Stack spacing={5} mx={'auto'} maxW={'700px'} w={'100%'} py={12} px={0}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'4xl'} textAlign={'center'} color={'gray.100'}>
+                            Add new interesting Point!
+                        </Heading>
+                        <Text fontSize={'lg'} color={'gray.400'}>
+                            save your favourite place 🌎
+                        </Text>
+                    </Stack>
+
+                    <InfoBadge text="1. Informations"/>
+
+                    <Box
+                        rounded={'lg'}
+                        bg={boxColor}
+                        boxShadow={'lg'}
+                        p={8}>
+
+                        <Stack spacing={5}>
+
+                            <FormControl isInvalid={errors.name}>
+                                <FormLabel htmlFor='name' color={labelColor}>Name</FormLabel>
+                                <Input id='name' type="text" color={inputColor} bgColor={subBoxColor}
+                                       placeholder='Give a wonderfull name'
+                                       {...register("name", {
+                                           required: "This is required",
+                                           minLength: {value: 3, message: "Minimum length should be 4"}
+                                       })} />
+                                <FormErrorMessage>
+                                    {errors.name && errors.name.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <FormControl isInvalid={errors.description}>
+                                <FormLabel htmlFor="description" color={labelColor}>Description</FormLabel>
+                                <InputGroup>
+                                    <Textarea id='description' color={inputColor} bgColor={subBoxColor}
+                                              placeholder="Write amazing description"
+                                              {...register("description", {
+                                                  required: "This is required",
+                                                  minLength: {value: 5, message: "Minimum length should be 5"}
+                                              })} />
+                                </InputGroup>
+                                <FormErrorMessage>
+                                    {errors.description && errors.description.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <FormControl isInvalid={errors.category}>
+                                <FormLabel htmlFor='category' color={labelColor}>Category</FormLabel>
+                                <Select id='category' color={inputColor} bgColor={subBoxColor}
+                                        placeholder='Select category'
+                                        {...register("category", {
+                                                required: "This is required"
+                                            }
+                                        )}>
+                                    {categoryList}
+                                </Select>
+                                <FormErrorMessage>
+                                    {errors.category && errors.category.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <FormControl isInvalid={errors.isPrivate} display='flex' alignItems='center'>
+                                <FormLabel htmlFor='isGlobal' mb='0' color={labelColor}>Global</FormLabel>
+                                <Switch id='isGlobal'
+                                        {...register("isGlobal", {})} />
+                                <FormErrorMessage>
+                                    {errors.isGlobal && errors.isGlobal.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                        </Stack>
+                    </Box>
+
+                    <InfoBadge text="2. Pick location"/>
+
+                    <Box
+                        rounded={'lg'}
+                        bg={boxColor}
+                        boxShadow={'lg'}
+                        p={8}>
+
+                        <Stack spacing={5}>
+                            <FormControl>
+                                <FormLabel htmlFor='findLocation' color={labelColor}>Location</FormLabel>
+                                <InputGroup>
+                                    <Input id="findLocation"
+                                           type="text"
+                                           color={inputColor}
+                                           value={value}
+                                           onChange={handleChange}
+                                           placeholder='Find location'
+                                    />
+                                    <InputRightElement h={'full'}>
+                                        <Button
+                                            variant={'ghost'}
+                                            onClick={handleClick}
+                                            color={'blue.300'}
+                                        >
+                                            <SearchIcon/>
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+
+                            <Collapse in={showPointList} animateOpacity>
+                                <Box
+                                    padding={"15px"}
+                                    color='white'
+                                    mt='4'
+                                    bg='blue.200'
+                                    rounded='md'
+                                    shadow='md'
+                                >
+                                    <TableContainer>
+                                        <Table variant='striped' colorScheme='teal'>
+                                            <Thead>
+                                                <Tr>
+                                                    <Th>Name</Th>
+                                                </Tr>
+                                            </Thead>
+                                            <Tbody>
+                                                {items.map(data => (
+                                                    <Tr>
+                                                        <Td>data.display_name</Td>
+                                                    </Tr>
+                                                ))}
+                                            </Tbody>
+                                        </Table>
+                                    </TableContainer>
+                                </Box>
+                            </Collapse>
+
+                            <Map height={'400px'} arr={items}/>
+
+                            <FormControl>
+                                <Input id='location' type="text" placeholder='ul. Sample 123' disabled={true}
+                                       value="Sample123"
+                                       {...register("location", {})} />
+                            </FormControl>
+
+                        </Stack>
+                    </Box>
+
+                    <Stack spacing={10} pt={2}>
+                        <Button
+                            loadingText="Submitting"
+                            size="lg"
+                            bg={'blue.600'}
+                            color={'white'}
+                            _hover={{
+                                bg: 'blue.500',
+                            }}
+                            width={'99%'}
+                            alignSelf={'center'}
+                            type="submit"
+                            isLoading={isSubmitting}
+                        >
+                            Add Point
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Flex>
+        </form>
+    );
 }
 
 // export default withAuthenticationRequired(AddPoint, {
--- socialmap.webapp/src/components/Map/Map.js
+++ socialmap.webapp/src/components/Map/Map.js
@@ -1,42 +1,52 @@
 import React, {useState} from "react";
-import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap} from 'react-leaflet';
 import './Map.js.css'
 import MapMarkers from "./MapMarkers";
 import {ReactComponent as Pin} from '../../icons/pin-icon.svg'
 import {ReactComponent as Arrow} from '../../icons/arrow-right.svg'
 import leaflet from 'leaflet'
 import {InputGroup, InputLeftElement, Input, InputRightElement, Button, Box} from "@chakra-ui/react";
+import {POIMock} from "../../mocks/POIMock";
+import {ReactComponent as Like} from "../../icons/like-icon.svg";
 
 function LocationMarker() {
-    const [position, setPosition] = useState(null)
     const map = useMapEvents({
-        click(event) {
-            new leaflet.marker(event.latlng).addTo(map);
-        },
-        locationfound(e) {
-            setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
-        },
+        moveend(event) {
+            console.log(map.getCenter())
+            return(map.getCenter())
+        }
     })
-
-    return position === null ? null : (
-        <Marker position={position}>
-            <Popup>You are here</Popup>
-        </Marker>
-    )
+    return null;
 }
 function Map(props) {
     const [value, setValue] = React.useState('')
     const [name, setName] = React.useState('')
+    const [position, setPosition] = React.useState('')
+    const [display, setDisplay] = React.useState(false)
     const handleChange = (event) => {
         setValue(event.target.value)
     }
     const handleClick = () => {
         setName(value)
     }
+    const getTempPoints = (points) => {
+        try {
+            if(points.features.length > 0) {
+                return(
+                    points.features.map(data => (
+                        <Marker key={1} position={[data.geometry.coordinates[1], data.geometry.coordinates[0]]}/>
+                    ))
+                );
+            } else {
+                return null;
+            }
+        } catch (err) {
+            return null;
+        }
+    }
     return (
         <Box className={"map-container"}>
-            <InputGroup className={"input-container"} minWidth={"300px"} width={"25%"} borderStyle={"solid"} borderColor={"#1A202C"}>
+            <InputGroup className={"input-container"} minWidth={"235px"} width={"25%"} borderStyle={"solid"} borderColor={"#1A202C"}>
                 <InputLeftElement
                     pointerEvents='none'
                     children={<Pin className={"pin-icon"} />}
@@ -56,13 +66,17 @@ function Map(props) {
                     </Button>
                 </InputRightElement>
             </InputGroup>
-            <MapContainer  className={"leaflet-container"} center={[ 52.22983, 21.01173 ]} zoom={12} scrollWheelZoom={true} height={'200px'} style={{height: props.heigh ? '100%' : props.height}}>
+            <MapContainer className={"leaflet-container"} center={[ 52.22983, 21.01173 ]} zoom={12} maxBounds={[
+                [52.31222, 20.81361],
+                [52.10302, 21.26336]
+            ]} scrollWheelZoom={true} height={'200px'} style={{height: props.height ? '100%' : props.height}}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
+                {getTempPoints(props.arr)}
                 {MapMarkers(name)}
-                {/*<LocationMarker />*/}
+                <LocationMarker/>
             </MapContainer>
         </Box>
         
