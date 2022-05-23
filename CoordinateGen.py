392d7f9eb24c80b135aa7dcd6d5813dfd612a904
Oskar <marchewjapko@gmail.com>
2022-05-23 20:01:41 +0200

    Added coordinates gen, so that I can see them

---
+++ CoordinateGen.py
@@ -0,0 +1,7 @@
+import random
+
+random1 = random.uniform(52.098, 52.368)
+random2 = random.uniform(20.852, 21.271)
+print('\"X\": ', random1, ",\n", '\"Y\": ', random2, ",\n", sep='')
+
+print(random1, random2)
