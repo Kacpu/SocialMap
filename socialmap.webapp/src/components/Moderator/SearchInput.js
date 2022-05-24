46512294c7c708da219cf9d2e1279e23249d2cf8
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-05-24 13:21:36 +0200

    SearchBar in scroll fix. Simple fix to add point

--- socialmap.webapp/src/components/Moderator/SearchInput.js
+++ socialmap.webapp/src/components/Moderator/SearchInput.js
@@ -6,15 +6,24 @@ export default function SearchInput(props) {
 
     const [inputValue, setInputValue] = useState();
 
+    function checkKey(event){
+        if(event.key == 'Enter'){
+            props.findFromInput(inputValue);
+        }
+    }
+
     function handle(event){
         setInputValue(event.target.value);
+        if(event.target.value.length == 0){
+            props.findFromInput(event.target.value);
+        }
         //console.log(event.target.value)
     }
 
     return (
         <Box>
             <InputGroup>
-                <Input bg={"gray.800"} placeholder={props.placeholder} onChange={handle}></Input>
+                <Input bg={"gray.800"} placeholder={props.placeholder} onChange={handle} onKeyDown={checkKey}></Input>
                 <Button leftIcon={<SearchIcon></SearchIcon>} width={"120px"} ml={"2"}
                         variant={"outline"} onClick={() => props.findFromInput(inputValue)}
                 >Find</Button>
