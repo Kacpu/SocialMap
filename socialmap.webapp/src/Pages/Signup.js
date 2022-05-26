a43a99b6f33b70735e0f5906b0517b160653cc90
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-05-26 16:01:26 +0200

    Quick fixes for authPanels and Edit Map

--- socialmap.webapp/src/Pages/Signup.js
+++ socialmap.webapp/src/Pages/Signup.js
@@ -1,8 +1,14 @@
 import SignupForm from '../auth/SignupForm'
 import SignupFormCustom from "../auth/SignupFormCustom";
+import {Button, Flex} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
 
 export default function Signup() {
+
+
     return (
-        <SignupForm />
+        <Flex justifyContent={"center"} alignItems={"center"} height={"100%"}>
+            <SignupForm/>
+        </Flex>
     );
 }
